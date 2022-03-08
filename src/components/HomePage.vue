e869b3cb83b6838f18b4fba3c47f572f77a6900f
Storm <erden.ouz@gmail.com>
2022-03-08 23:40:35 +0300

    first commit

---
+++ src/components/HomePage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <h1>Hello world</h1>
+  </div>
+</template>
+
+<script>
+import italianSentences from '@/assets/italiano.json'
+export default {
+  name: 'HomePage',
+  data() {
+    return {
+      sentences: [],
+      italianSentences: italianSentences,
+    }
+  },
+  mounted() {
+    this.getSentences()
+    console.log(this.sentences)
+  },
+  methods: {
+    getSentences() {
+      this.sentences = this.italianSentences
+    },
+  },
+}
+</script>
